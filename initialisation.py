cebf238cb6da87eaae7dc2d71e0806e857936aa1
NZaman02 <96191360+NZaman02@users.noreply.github.com>
2023-03-25 23:16:00 +0000

    improved code quality

--- initialisation.py
+++ initialisation.py
@@ -7,15 +7,11 @@ def intialiseOurGoal(environmentDict):
     G42coords = environmentDict["G42"]
     G43coords = environmentDict["G43"]
 
-    xdist42 = Math.abs(M8coords[0] - G42coords[0])
-    ydist42 = Math.abs(M8coords[1] - G42coords[1])
+    def euclidean_distance(x1, y1, x2, y2):
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-    xdist43 = Math.abs(M8coords[0] - G43coords[0])
-    ydist43 = Math.abs(M8coords[1] - G43coords[1])
-
-    #calcs closer goal
-    euclid42 = math.sqrt( (xdist42**2) + (ydist42**2) )
-    euclid43 = math.sqrt( (xdist43**2) + (ydist43**2) )
+    euclid42 = euclidean_distance(M8coords[0], M8coords[1], G42coords[0], G42coords[1]):
+    euclid43 = euclidean_distance(M8coords[0], M8coords[1], G43coords[0], G43coords[1]):
 
     #chooses our goal
     if euclid42 > euclid43:
