55d3b4a8854ca33eafa7b74ee9d1085a441a7716
Thomas Leitch <thomas.leitch@student.manchester.ac.uk>
2023-03-26 06:20:04 +0100

    arduino

--- nano.ino
+++ nano.ino
@@ -13,95 +13,36 @@
 #include <SPI.h>
 #include <RF24.h>
 
-
 // instantiate an object for the nRF24L01 transceiver
 RF24 radio(7, 8);  // using pin 7 for the CE pin, and pin 8 for the CSN pin
 
-// Let these addresses be used for the pair
 const byte address[6] = "00001"; // Define the address of the receiving NRF24L01+ module.
-// It is very helpful to think of an address as a path instead of as
-// an identifying device destination
-
-// to use different addresses on a pair of radios, we need a variable to
-// uniquely identify which address this radio will use to transmit
-bool radioNumber = 1;  // 0 uses address[0] to transmit, 1 uses address[1] to transmit
-
-// Used to control whether this node is sending or receiving
-bool role = true;  // true = TX role, false = RX role
-
-// For this example, we'll be using a payload containing
-// a single float number that will be incremented
-// on every successful transmission
 
 void setup() {
 
   Serial.begin(115200);
-  while (!Serial) {
-    // some boards need to wait to ensure access to serial over USB
-  }
+  while (!Serial) {}
   // initialize the transceiver on the SPI bus
   if (!radio.begin()) {
     Serial.println(F("radio hardware is not responding!!"));
     while (1) {}  // hold in infinite loop
   }
-  /*
-  // print example's introductory prompt
-  Serial.println(F("RF24/examples/GettingStarted"));
 
-  // To set the radioNumber via the Serial monitor on startup
-  Serial.println(F("Which radio is this? Enter '0' or '1'. Defaults to '0'"));
-  while (!Serial.available()) {
-    // wait for user input
-  }
-  char input = Serial.parseInt();
-  radioNumber = input == 1;
-  Serial.print(F("radioNumber = "));
-  Serial.println((int)radioNumber);
-
-  // role variable is hardcoded to RX behavior, inform the user of this
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
-  */
-  // Set the PA Level low to try preventing power supply related problems
-  // because these examples are likely run with nodes in close proximity to
-  // each other.
-  //radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
-
-  // save on transmission time by setting the radio to only transmit the
-  // number of bytes we need to transmit a float
-  //radio.setPayloadSize(15);  // float datatype occupies 4 bytes
-
-  // set the TX address of the RX node into the TX pipe
+  radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
   radio.openWritingPipe(address);  // always uses pipe 0
-  radio.setDataRate(RF24_2MBPS);
-  // additional setup specific to the node's role
   radio.stopListening();  // put radio in TX mode
-
-  // For debugging info
-  // printf_begin();             // needed only once for printing details
-  // radio.printDetails();       // (smaller) function that prints raw register values
-  // radio.printPrettyDetails(); // (larger) function that prints human readable data
-
-}  // setup
+} 
 
 void loop() {
     // change the role via the serial monitor
-    String inputString = Serial.readString(); // read the string from Serial
-    if (inputString.length()>0){
-      inputString=inputString.substring(0,9);
-      //Serial.print(inputString);
-      long sen = atol(inputString.c_str());
-      Serial.println(sen);
-      bool report = radio.write(&sen, sizeof(long));  // transmit & save the report
-      /*
-      if (report){
-        Serial.print("All good in the ");
-      }else{
-        Serial.print("All bad in the hood");
+    if (Serial.available() > 0)  {
+      long message = Serial.parseInt();
+//      Serial.print("Sending value: ");
+//      Serial.println(message);
+      bool report = radio.write(&message, sizeof(long));
+      if (report) {
+        Serial.println("Transmited " + String(message) + " successfully! ");
       }
-      */
-      //delay(50);  // slow transmissions down by 1 second
     }
-
-  
-
+    delay(50);
 }  // loop
--- sketch_mar25a.ino
+++ sketch_mar25a.ino
@@ -14,27 +14,18 @@ int Motor_left_PWM = 9;    //   0 (min speed) - 255 (max speed)
 int Motor_left_direction = 6;   //   0 Forward - 1 Reverse
 #define Forward 0
 #define Reverse 1
-// LED
-int LED1 = 13;
-int LED2 = 12;
-int IR_enable=4;
-int IR_threshold= 900; // 0 white close obstacle -- 1023 no obstacle
-
 
 void setup() {
-// initialize serial communication at 9600 bits per second:
- Serial.begin(115200);
-// initialize Ports
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(115200);
+  // initialize Ports
   pinMode(Motor_left_PWM, OUTPUT);
   pinMode(Motor_right_PWM, OUTPUT);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(IR_enable, OUTPUT);
   radio.begin();
   // Set the PA Level low to try preventing power supply related problems
   // because these examples are likely run with nodes in close proximity to
   // each other.
-  //radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
 
   // save on transmission time by setting the radio to only transmit the
   // number of bytes we need to transmit a float
@@ -42,104 +33,10 @@ void setup() {
 
   // set the RX address of the TX node into a RX pipe
   radio.openReadingPipe(1, address);  // using pipe 1
-   radio.setDataRate(RF24_2MBPS);
+  //radio.setDataRate(RF24_2MBPS);
   // additional setup specific to the node's role
   radio.startListening();  // put radio in RX mo
 }
-
-void forward(){
-  analogWrite(Motor_right_PWM,120); // right motor
-  digitalWrite(Motor_right_direction,Forward); //right
-  analogWrite(Motor_left_PWM, 120); // left 
-  digitalWrite(Motor_left_direction,Forward); //left
-}
-
-void reverse(int delay_time){
-  analogWrite(Motor_right_PWM,120 ); // right motor
-  digitalWrite(Motor_right_direction,Reverse); //right
-  analogWrite(Motor_left_PWM, 120); // left 
-  digitalWrite(Motor_left_direction,Reverse); //left
-  delay(delay_time);
-}
-
-void right(int delay_time){
-  reverse(50);
-  analogWrite(Motor_right_PWM,120 ); // right motor
-  digitalWrite(Motor_right_direction,Reverse); //right
-  analogWrite(Motor_left_PWM, 40); // left 
-  digitalWrite(Motor_left_direction,Forward); //left
-  delay(delay_time);
-}
-
-void left(int delay_time){
-  reverse(50);
-  analogWrite(Motor_right_PWM,40 ); // right motor
-  digitalWrite(Motor_right_direction,Forward); //right
-  analogWrite(Motor_left_PWM, 120); // left 
-  digitalWrite(Motor_left_direction,Reverse); //left
-  delay(delay_time);
-}
-
-// Variables for 5 IR proximity sensors 
-int IR_right,IR_right_front,IR_front,IR_left_front,IR_left;
-
-void IR_proximity_read(){    // read IR sensors 
-  int n=5;  // average parameter
-  digitalWrite(IR_enable, HIGH);  //IR Enable
-  IR_right=0;
-  IR_right_front=0;
-  IR_front=0;
-  IR_left_front=0;
-  IR_left=0;
-  for (int i=0;i<n;i++){
-    IR_right+=analogRead(A3);
-    IR_right_front+=analogRead(A2);
-    IR_front+=analogRead(A1);
-    IR_left_front+=analogRead(A0);
-    IR_left+=analogRead(A7);
-    delay(5);
-  }
-  IR_right/=n;
-  IR_right_front/=n;
-  IR_front/=n;
-  IR_left_front/=n;
-  IR_left/=n; 
-}
-
-// obstacle avoidance
-void Obstacle_avoidance(){
-  if (IR_front<IR_threshold){
-      digitalWrite(LED2,HIGH);
-      reverse(300);
-      right(500);
-      digitalWrite(LED2,LOW);
-  }
-  if (IR_right<IR_threshold || IR_right_front<IR_threshold ){
-      digitalWrite(LED2,HIGH);
-      right(500);
-      digitalWrite(LED2,LOW);
-  } else {
-      if (IR_left<IR_threshold || IR_left_front<IR_threshold ){
-          digitalWrite(LED2,HIGH);
-          left(500);
-          digitalWrite(LED2,LOW);
-      } else forward();
-  }  
-}
-
-// send IR readings to Serial Monitor
-void Send_sensor_readings(){
- Serial.print(IR_right);
- Serial.print(',');
- Serial.print(IR_right_front);
- Serial.print(',');
- Serial.print(IR_front);
- Serial.print(',');
- Serial.print(IR_left_front);
- Serial.print(',');
- Serial.println(IR_left);  
-}
-
 
 // the loop routine runs over and over again forever:
 void loop() {
@@ -148,23 +45,22 @@ void loop() {
       //uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
       long data;
       radio.read(&data, sizeof(long));
+      Serial.println(data);
       String receivedString = String(data);
       //receivedString.trim(); // remove any leading/trailing white space
-      //Serial.println(receivedString);
-      //Serial.println(receivedString);
       int values[5]; // array to store the split values
       values[0] = receivedString[0]- '0';
       values[1] = receivedString[1]- '0';
       values[2] = receivedString[2]- '0';
       values[3] = receivedString.substring(3,6).toInt();
       values[4] = receivedString.substring(6,9).toInt();
-      //Serial.print(values[0]);Serial.print(",");
-      //Serial.print(values[1]);Serial.print(",");
-      //Serial.print(values[2]);Serial.print(",");
-      //Serial.print(values[3]);Serial.print(",");
-      //Serial.print(values[4]);Serial.println(",");
-      if (values[0]==8){
-        //Serial.print("I'm moving!");
+      Serial.print(values[0]);Serial.print(",");
+      Serial.print(values[1]);Serial.print(",");
+      Serial.print(values[2]);Serial.print(",");
+      Serial.print(values[3]);Serial.print(",");
+      Serial.print(values[4]);Serial.println(",");
+      if (values[0]==7){
+        Serial.print("I'm moving!");
         analogWrite(Motor_right_PWM,values[4] ); // right motor
         digitalWrite(Motor_right_direction,values[2]); //right
         analogWrite(Motor_left_PWM, values[3]); // left 
@@ -177,7 +73,7 @@ void loop() {
       Serial.print(F(": "));
       Serial.println(payload);  // print the payload's value
       */
-      //delay(50);        // delay in between reads for stability
+      delay(50);        // delay in between reads for stability
     }
  
 }
